03f543b80f3036855886b3afc18082fabcb5f683
Bruno Wagner <bruno.wagner.b.r@gmail.com>
2022-07-19 12:57:11 -0300

    refactor: tabela list com autoincrement e valores padrões

--- docker-compose/mysql/imdsound.sql
+++ docker-compose/mysql/imdsound.sql
@@ -48,9 +48,9 @@ DROP TABLE IF EXISTS `album`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `album` (
   `list_id_list` int(11) NOT NULL,
-  `data_cadastro` date NOT NULL,
+  `data_cadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`list_id_list`),
-  CONSTRAINT `fk_Album_List1` FOREIGN KEY (`list_id_list`) REFERENCES `list` (`id_list`) ON DELETE CASCADE ON UPDATE RESTRICT
+  CONSTRAINT `fk_Album_List1` FOREIGN KEY (`list_id_list`) REFERENCES `list` (`id_list`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -60,6 +60,7 @@ CREATE TABLE `album` (
 
 LOCK TABLES `album` WRITE;
 /*!40000 ALTER TABLE `album` DISABLE KEYS */;
+INSERT INTO `album` VALUES (1,'2022-07-19 15:22:29');
 /*!40000 ALTER TABLE `album` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,8 +77,8 @@ CREATE TABLE `album_has_music` (
   PRIMARY KEY (`album_list_id_list`,`music_id_music`),
   KEY `fk_Album_has_music_music1_idx` (`music_id_music`),
   KEY `fk_Album_has_music_Album1_idx` (`album_list_id_list`),
-  CONSTRAINT `fk_Album_has_music_Album1` FOREIGN KEY (`album_list_id_list`) REFERENCES `album` (`list_id_list`) ON DELETE CASCADE ON UPDATE RESTRICT,
-  CONSTRAINT `fk_Album_has_music_music1` FOREIGN KEY (`music_id_music`) REFERENCES `music` (`idmusic`) ON DELETE CASCADE ON UPDATE RESTRICT
+  CONSTRAINT `fk_Album_has_music_Album1` FOREIGN KEY (`album_list_id_list`) REFERENCES `album` (`list_id_list`) ON DELETE CASCADE,
+  CONSTRAINT `fk_Album_has_music_music1` FOREIGN KEY (`music_id_music`) REFERENCES `music` (`idmusic`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -101,11 +102,11 @@ CREATE TABLE `artist` (
   `user_email` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
   `description` varchar(45) NOT NULL,
-  `admin_id_admin` int(11),
+  `admin_id_admin` int(11) DEFAULT NULL,
   PRIMARY KEY (`user_email`),
   KEY `fk_Artist_Admin1_idx` (`admin_id_admin`),
   KEY `fk_Artist_User1_idx` (`user_email`),
-  CONSTRAINT `fk_Artist_Admin1` FOREIGN KEY (`admin_id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE SET NULL ON UPDATE RESTRICT,
+  CONSTRAINT `fk_Artist_Admin1` FOREIGN KEY (`admin_id_admin`) REFERENCES `admin` (`id_admin`) ON DELETE SET NULL,
   CONSTRAINT `fk_Artist_User1` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -133,7 +134,7 @@ CREATE TABLE `artist_cadastra_album` (
   PRIMARY KEY (`artist_user_email`,`album_list_id_list`),
   KEY `fk_Artist_has_Album_Album1_idx` (`album_list_id_list`),
   KEY `fk_Artist_has_Album_Artist1_idx` (`artist_user_email`),
-  CONSTRAINT `fk_Artist_has_Album_Album1` FOREIGN KEY (`album_list_id_list`) REFERENCES `album` (`list_id_list`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `fk_Artist_has_Album_Album1` FOREIGN KEY (`album_list_id_list`) REFERENCES `album` (`list_id_list`) ON DELETE CASCADE,
   CONSTRAINT `fk_Artist_has_Album_Artist1` FOREIGN KEY (`artist_user_email`) REFERENCES `artist` (`user_email`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -144,6 +145,7 @@ CREATE TABLE `artist_cadastra_album` (
 
 LOCK TABLES `artist_cadastra_album` WRITE;
 /*!40000 ALTER TABLE `artist_cadastra_album` DISABLE KEYS */;
+INSERT INTO `artist_cadastra_album` VALUES ('bruno@gmail.com',1);
 /*!40000 ALTER TABLE `artist_cadastra_album` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -161,7 +163,7 @@ CREATE TABLE `artist_cadastra_music` (
   KEY `fk_Artist_has_music_music1_idx` (`music_id_music`),
   KEY `fk_Artist_has_music_Artist1_idx` (`artist_user_email`),
   CONSTRAINT `fk_Artist_has_music_Artist1` FOREIGN KEY (`artist_user_email`) REFERENCES `artist` (`user_email`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_Artist_has_music_music1` FOREIGN KEY (`music_id_music`) REFERENCES `music` (`idmusic`) ON DELETE CASCADE ON UPDATE RESTRICT
+  CONSTRAINT `fk_Artist_has_music_music1` FOREIGN KEY (`music_id_music`) REFERENCES `music` (`idmusic`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -206,13 +208,13 @@ DROP TABLE IF EXISTS `list`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `list` (
-  `id_list` int(11) NOT NULL,
+  `id_list` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
-  `num_likes` int(11) NOT NULL,
+  `num_likes` int(11) NOT NULL DEFAULT '0',
   `duration_time` int(11) NOT NULL DEFAULT '0',
   `picture` mediumtext,
   PRIMARY KEY (`id_list`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -221,6 +223,7 @@ CREATE TABLE `list` (
 
 LOCK TABLES `list` WRITE;
 /*!40000 ALTER TABLE `list` DISABLE KEYS */;
+INSERT INTO `list` VALUES (1,'Primeiro',0,0,NULL);
 /*!40000 ALTER TABLE `list` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -237,8 +240,8 @@ CREATE TABLE `list_has_genre` (
   PRIMARY KEY (`genre_name`,`list_id_list`),
   KEY `fk_List_has_genre_genre1_idx` (`genre_name`),
   KEY `fk_List_has_genre_List1_idx` (`list_id_list`),
-  CONSTRAINT `fk_List_has_genre_List1` FOREIGN KEY (`list_id_list`) REFERENCES `list` (`id_list`) ON DELETE CASCADE ON UPDATE RESTRICT,
-  CONSTRAINT `fk_List_has_genre_genre1` FOREIGN KEY (`genre_name`) REFERENCES `music_genre` (`name`) ON DELETE RESTRICT ON UPDATE CASCADE
+  CONSTRAINT `fk_List_has_genre_List1` FOREIGN KEY (`list_id_list`) REFERENCES `list` (`id_list`) ON DELETE CASCADE,
+  CONSTRAINT `fk_List_has_genre_genre1` FOREIGN KEY (`genre_name`) REFERENCES `music_genre` (`name`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -297,11 +300,7 @@ CREATE TABLE `music_genre` (
 
 LOCK TABLES `music_genre` WRITE;
 /*!40000 ALTER TABLE `music_genre` DISABLE KEYS */;
-INSERT INTO `music_genre` VALUES ('pass1');
-INSERT INTO `music_genre` VALUES ('pass2');
-INSERT INTO `music_genre` VALUES ('pass3');
-INSERT INTO `music_genre` VALUES ('pass4');
-INSERT INTO `music_genre` VALUES ('pass5');
+INSERT INTO `music_genre` VALUES ('pass1'),('pass2'),('pass3'),('pass4'),('pass5');
 /*!40000 ALTER TABLE `music_genre` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -318,8 +317,8 @@ CREATE TABLE `music_has_genre` (
   PRIMARY KEY (`music_idmusic`,`genre_name`),
   KEY `fk_music_has_genre_genre1_idx` (`genre_name`),
   KEY `fk_music_has_genre_music1_idx` (`music_idmusic`),
-  CONSTRAINT `fk_music_has_genre_genre1` FOREIGN KEY (`genre_name`) REFERENCES `music_genre` (`name`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT `fk_music_has_genre_music1` FOREIGN KEY (`music_idmusic`) REFERENCES `music` (`idmusic`) ON DELETE CASCADE ON UPDATE RESTRICT
+  CONSTRAINT `fk_music_has_genre_genre1` FOREIGN KEY (`genre_name`) REFERENCES `music_genre` (`name`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_music_has_genre_music1` FOREIGN KEY (`music_idmusic`) REFERENCES `music` (`idmusic`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -344,7 +343,7 @@ CREATE TABLE `playlist` (
   `visibilty` tinyint(4) NOT NULL,
   PRIMARY KEY (`list_id_list`),
   KEY `fk_Playlist_List1_idx` (`list_id_list`),
-  CONSTRAINT `fk_Playlist_List1` FOREIGN KEY (`list_id_list`) REFERENCES `list` (`id_list`) ON DELETE CASCADE ON UPDATE RESTRICT
+  CONSTRAINT `fk_Playlist_List1` FOREIGN KEY (`list_id_list`) REFERENCES `list` (`id_list`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -370,8 +369,8 @@ CREATE TABLE `playlist_has_music` (
   PRIMARY KEY (`playlist_list_id_list`,`music_id_music`),
   KEY `fk_Playlist_has_music_music1_idx` (`music_id_music`),
   KEY `fk_Playlist_has_music_Playlist1_idx` (`playlist_list_id_list`),
-  CONSTRAINT `fk_Playlist_has_music_Playlist1` FOREIGN KEY (`playlist_list_id_list`) REFERENCES `playlist` (`list_id_list`) ON DELETE CASCADE ON UPDATE RESTRICT,
-  CONSTRAINT `fk_Playlist_has_music_music1` FOREIGN KEY (`music_id_music`) REFERENCES `music` (`idmusic`) ON DELETE CASCADE ON UPDATE RESTRICT
+  CONSTRAINT `fk_Playlist_has_music_Playlist1` FOREIGN KEY (`playlist_list_id_list`) REFERENCES `playlist` (`list_id_list`) ON DELETE CASCADE,
+  CONSTRAINT `fk_Playlist_has_music_music1` FOREIGN KEY (`music_id_music`) REFERENCES `music` (`idmusic`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -401,7 +400,7 @@ CREATE TABLE `subscription` (
   KEY `fk_subscription_type1_idx` (`type_type_name`),
   KEY `fk_subscription_User1_idx` (`user_email`),
   CONSTRAINT `fk_subscription_User1` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_subscription_type1` FOREIGN KEY (`type_type_name`) REFERENCES `type` (`type_name`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  CONSTRAINT `fk_subscription_type1` FOREIGN KEY (`type_type_name`) REFERENCES `type` (`type_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -451,8 +450,8 @@ CREATE TABLE `type_has_feature` (
   PRIMARY KEY (`type_type_name`,`feature_feat_name`),
   KEY `fk_type_has_feature_feature1_idx` (`feature_feat_name`),
   KEY `fk_type_has_feature_type1_idx` (`type_type_name`),
-  CONSTRAINT `fk_type_has_feature_feature1` FOREIGN KEY (`feature_feat_name`) REFERENCES `feature` (`feat_name`) ON DELETE CASCADE ON UPDATE RESTRICT,
-  CONSTRAINT `fk_type_has_feature_type1` FOREIGN KEY (`type_type_name`) REFERENCES `type` (`type_name`) ON DELETE CASCADE ON UPDATE RESTRICT
+  CONSTRAINT `fk_type_has_feature_feature1` FOREIGN KEY (`feature_feat_name`) REFERENCES `feature` (`feat_name`) ON DELETE CASCADE,
+  CONSTRAINT `fk_type_has_feature_type1` FOREIGN KEY (`type_type_name`) REFERENCES `type` (`type_name`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -505,7 +504,7 @@ CREATE TABLE `user_has_playlist` (
   PRIMARY KEY (`user_email`,`playlist_list_id_list`),
   KEY `fk_User_has_Playlist_Playlist1_idx` (`playlist_list_id_list`),
   KEY `fk_User_has_Playlist_User1_idx` (`user_email`),
-  CONSTRAINT `fk_User_has_Playlist_Playlist1` FOREIGN KEY (`playlist_list_id_list`) REFERENCES `playlist` (`list_id_list`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `fk_User_has_Playlist_Playlist1` FOREIGN KEY (`playlist_list_id_list`) REFERENCES `playlist` (`list_id_list`) ON DELETE CASCADE,
   CONSTRAINT `fk_User_has_Playlist_User1` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -534,7 +533,7 @@ CREATE TABLE `user_listen_music` (
   KEY `fk_User_has_music_music1_idx` (`music_id_music`),
   KEY `fk_User_has_music_User1_idx` (`user_email`),
   CONSTRAINT `fk_User_has_music_User1` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_User_has_music_music1` FOREIGN KEY (`music_id_music`) REFERENCES `music` (`idmusic`) ON DELETE CASCADE ON UPDATE RESTRICT
+  CONSTRAINT `fk_User_has_music_music1` FOREIGN KEY (`music_id_music`) REFERENCES `music` (`idmusic`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -556,4 +555,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-16 20:01:41
+-- Dump completed on 2022-07-19 15:53:08
